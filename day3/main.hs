74f244affd566bc3e9e25069cb4b692f990566a4
conilas <meinen.nicolas@gmail.com>
2020-12-09 08:54:33 +0100

    day 3 b

--- day3/main.hs
+++ day3/main.hs
@@ -2,6 +2,8 @@ import qualified Data.Text    as Text
 import qualified Data.Text.IO as Text
 import qualified Data.Text.Read as Text
 
+data Slope = S Int Int
+
 readLines :: FilePath -> IO [String]
 readLines = fmap lines . readFile
 
@@ -17,15 +19,23 @@ add value (Just True) = value + 1
 add value (Just False) = value 
 add value Nothing = value
 
-walk (x:xs) horizontal acc =
+vertical :: [a] -> Int -> [a]
+vertical [] _ = []
+vertical (x:xs) 1 = xs 
+vertical (x:xs) n = vertical xs (n-1)
+
+walk (x:xs) horizontal acc (S v_walk h_walk) =
   let current_value = lookupValue (Text.unpack x) 0 horizontal (Text.unpack x) in 
-    let next_horiz = horizontal + 3 in
+    let next_horiz = horizontal + h_walk  in
+      let vertical_rest = vertical (x:xs) v_walk in
       let eq = fmap (\x -> x == '#') current_value in
         let acc_count = add acc eq in
-          walk xs next_horiz acc_count
-walk [] x acc = acc 
+          walk vertical_rest next_horiz acc_count (S v_walk h_walk) 
+walk [] x acc _ = acc 
 
 main = do
   ls <- fmap Text.lines (Text.readFile "input")
-  let walked = walk ls 0 0
-  print walked 
+  let partial_walkable = walk ls 0 0
+  let slope = [(S 1 1), (S 1 3), (S 1 5), (S 1 7), (S 2 1)]
+  let final = (foldr (*) 1) . map partial_walkable 
+  print (final slope)
