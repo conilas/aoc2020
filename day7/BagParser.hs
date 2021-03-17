b4a1aece97cae64b9633edb5f8ede606dce029b6
conilas <meinen.nicolas@gmail.com>
2021-03-17 18:33:09 +0100

    create parser for all the content - now we need to recursively resolve

--- day7/BagParser.hs
+++ day7/BagParser.hs
@@ -4,26 +4,45 @@ import qualified Data.Text    as Text
 import qualified Data.Text.IO as Text
 import qualified Data.Text.Read as Text
 import qualified Data.Map as Map
-import Text.Parsec (ParseError)
+import Text.Parsec (ParseError, try)
 import Text.Parsec.String (Parser)
+import Text.ParserCombinators.Parsec.Char (oneOf, char, string, digit, satisfy)
+import Text.Parsec.Combinator (many1, choice, chainl1)
 import Control.Applicative ((<|>), many)
 import Control.Monad (void)
 import Data.Char (isLetter, isDigit)
-import Text.ParserCombinators.ReadP
 import Data.Map (Map)
 import Data.List
 
-data Bag = Children String | ParentBag String [Bag]
+data Bag = Children String | ParentBag String [Bag] deriving Show
 
-bg = ParentBag "beige" [(Children "blue"), (Children "yellow")]
+whitespace :: Parser ()
+whitespace = void $ many $ oneOf " \n\t"
 
-isVowel :: Char -> Bool
-isVowel char =
-    any (char ==) "aouei"
+children :: Parser Bag
+children = do
+    void $ many1 whitespaceOrSeparator
+    void $ many1 digit
+    whitespace
+    color1 <- many1 letters 
+    whitespace
+    color2 <- many1 letters 
+    whitespace
+    _ <- try (string "bags") <|> string "bag"
+    return (Children (color1 ++ color2))
+  where
+    letters = satisfy isLetter
+    whitespaceOrSeparator = satisfy (\x -> x == ' ' || x == ',') 
+    
 
-vowel :: ReadP Char
-vowel =
-    satisfy isVowel
-
-
-dat = readP_to_S vowel "e"
+parens :: Parser Bag 
+parens = do
+    color1 <- many1 letters 
+    whitespace
+    color2 <- many1 letters 
+    whitespace
+    void $ string "bags contain"
+    childrens <- try (many children) <|> return [] 
+    return (ParentBag (color1 ++ color2) childrens)
+  where
+    letters = satisfy isLetter
--- day7/Main.hs
+++ day7/Main.hs
@@ -1,7 +1,25 @@
 module Main where
 
+import qualified Data.Text    as Text
+import qualified Data.Text.IO as Text
+import qualified Data.Text.Read as Text
+import Data.List
 import BagParser
+import Text.Parsec (parse, ParseError)
+import Text.Parsec.String (Parser)
+
+regularParse :: Parser a -> String -> Either ParseError a
+regularParse p = parse p ""
+
+printParsed [] = print "\n"
+printParsed (x:xs) = do
+  print x
+  printParsed xs
 
 main :: IO ()
 main = do
-  print (isVowel 'a')
+  ls <- fmap Text.lines (Text.readFile "input")
+  let mapped = map ((regularParse parens) . Text.unpack) ls 
+  printParsed mapped
+--  print (regularParse parens "plaid beige bags contain 3 drab magenta bags, 4 dull indigo bags.")
+--  print (regularParse parens "plaid beige bags contain 3 drab magenta bags.")
